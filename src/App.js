8814c5653a04dbe5d12dbcfab873c20a18db5b2a
ReactDeveloperVasim <reactdevloper.vasim@gmail.com>
2023-10-18 16:27:04 +0530

    proj2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>School Days</h1>
+      <div className="sub">
+        <h4>School Friends</h4>
+        <ol>
+        <li>Rahul</li>
+        <li>Sumit</li>
+        <li>Samir</li>
+        <li>Firoz</li>
+        </ol>
+      </div>
+      <div className="sub">
+        <h4>School Routine</h4>
+        <ol type="A">
+          <li>I used to Woke Up Early In the Morning</li>
+          <li>I used to Brush My Teeth</li>
+          <li>I used to take bath</li>
+          <li>I used to Take breakfast</li>
+          <li>I used to Catch My school Bus</li>
+          <li>I used to Enjoy My Day at School</li>
+          <li>I used to catch bus to come back to home</li>
+        </ol>
+      </div>
+      <div className="sub">
+            <h4>School Games</h4>
+            <ol>
+              <li>Cricket</li>
+              <li>Football</li>
+              <li>VolleyBall</li>
+              <li>Kabaddi</li>
+              <li>Kho-Kho</li>
+            </ol>
+      </div>
+      <div className="sub">
+        <h4>School Books</h4>
+        <ol>
+          <li>Marathi</li>
+          <li>Hindi</li>
+          <li>English</li>
+          <li>Math</li>
+          <li>Science</li>
+          <li>Social Science</li>
+          <li>Urdu</li>
+        </ol>
+      </div>
     </div>
   );
 }
